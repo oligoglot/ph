b2ef91314b45d92e7da35e52195c1a831ce229b0
Sundar <quasilinguist@gmail.com>
2021-04-02 12:36:19 +0530

    adding pa initial filter

--- KannadaLitmusEngine.py
+++ KannadaLitmusEngine.py
@@ -1,25 +1,37 @@
 import xml.etree.ElementTree as ET
 import sys
+import re
 
 
 class KannadaLitmusEngine:
 
     def __init__(self):
-        pass
+        self.noise_pattern = re.compile(r'\=\=\=?[^\=]+\=?\=\=')
+        self.p_initials = re.compile(r'^\u0caa[\u0CBE-\u0ce3]')
 
     def parse_xml(self, f):
         root = ET.parse(f)
-        docs = root.findall('./doc')
-        return docs
+        for doc in root.findall('./doc'):
+            yield doc
 
     def get_candidates(self, doc):
-        return doc.findall('./title')
+        title = doc.findall('./title')
+        abstract = doc.findall('./abstract')
+        return abstract
+
+    def get_synonyms(self, abstract):
+        if abstract:
+            abstract = self.noise_pattern.sub(' ', abstract)
+            words = list(filter(lambda w: self.p_initials.match(w), re.split(r'\W+', abstract)))
+            return words
+        return []
 
 
 if __name__ == "__main__":
     f = sys.argv[1]
     e = KannadaLitmusEngine()
-    docs = e.parse_xml(f)
-    for doc in docs:
-        for c in e.get_candidates(doc):
-            print(c.text)
+    for doc in e.parse_xml(f):
+        for abstract in e.get_candidates(doc):
+            syns = e.get_synonyms(abstract.text)
+            if len(syns) > 0:
+                print(syns)
