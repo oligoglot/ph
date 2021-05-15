77d0ed78ae539019265071082757bb9969e9e1cd
Sundar <quasilinguist@gmail.com>
2021-05-15 19:49:40 +0530

    add presumable synonyms and separate unpaired based on initial

--- KannadaLitmusEngine.py
+++ KannadaLitmusEngine.py
@@ -14,6 +14,9 @@ class KannadaLitmusEngine:
         self.h_initials = re.compile('^(ಹ|ಹಾ|ಹಿ|ಹೀ|ಹು|ಹೂ|ಹೆ|ಹೇ|ಹೈ|ಹೊ|ಹೋ|ಹೌ)')
         self.ha = re.compile('^ಹ')
         self.pertinent_words = set()
+        self.hertinent_words = set()
+        self.psuffixes = set()
+        self.hsuffixes = set()
         # self.load_titles()
 
     def load_titles(self):
@@ -56,9 +59,11 @@ class KannadaLitmusEngine:
                         word = re.sub(self.pa, '', word, 1)
                         pwords.add(word)
                     elif self.h_initials.match(word):
-                        self.pertinent_words.add(word)
+                        self.hertinent_words.add(word)
                         word = re.sub(self.ha, '', word, 1)
                         hwords.add(word)
+                self.psuffixes.update(pwords)
+                self.hsuffixes.update(hwords)
                 phsynonyms = list(map(lambda suffix: (
                     'ಪ' + suffix, 'ಹ' + suffix), set.intersection(pwords, hwords)))
                 return phsynonyms
@@ -81,8 +86,18 @@ if __name__ == "__main__":
     with open("phsynonyms.csv", "w") as synf:
         for pair in pairs:
             print(pair, file=synf)
-    with open("phuniverse.csv", "w") as unif, open("unpaired.csv", "w") as unpairedf:
+    with open("presumable_synonyms.csv", "w") as psynf:
+        presumable_synonyms = e.psuffixes.intersection(e.hsuffixes)
+        for suffix in presumable_synonyms:
+            print('ಪ' + suffix + ',' + 'ಹ' + suffix, file=psynf)
+    with open("puniverse.csv", "w") as punif, open("huniverse.csv", "w") as hunif, open("unpaired.csv", "w") as unpairedf:
         for word in e.pertinent_words:
-            print(word, file=unif)
-            if word not in pairedwords:
+            suffix = re.sub(e.pa, '', word)
+            print(word + ',' + suffix, file=punif)
+            if word not in pairedwords and suffix not in presumable_synonyms:
+                print(word, file=unpairedf)
+        for word in e.hertinent_words:
+            suffix = re.sub(e.ha, '', word)
+            print(word + ',' + suffix, file=hunif)
+            if word not in pairedwords and suffix not in presumable_synonyms:
                 print(word, file=unpairedf)
